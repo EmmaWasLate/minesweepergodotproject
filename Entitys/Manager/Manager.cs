a68dd84c7cbcd67a21116b9fff00fb3d5dfc81e5
EmmaWasLate <luizgustavohain2007@gmail.com>
2026-01-27 00:04:27 -0300

    scaling code

--- Entitys/Manager/Manager.cs
+++ Entitys/Manager/Manager.cs
@@ -6,11 +6,19 @@ public partial class Manager : Node2D
 	[Export] public TileMapLayer tilemaplayer;
 	public override void _Ready()
 	{
+		float xScaling = (DisplayServer.WindowGetSize().X - 100) / (Singleton.MapSizes.easy.X * 16);
+		float yScaling = (DisplayServer.WindowGetSize().Y - 100) / (Singleton.MapSizes.easy.Y * 16);
 		
-		
+		if (xScaling < yScaling)
+		{
+			tilemaplayer.Scale = new(xScaling, xScaling);
+		} else
+		{
+			tilemaplayer.Scale = new(yScaling, yScaling);
+		}
 
+		tilemaplayer.Position = new (DisplayServer.WindowGetSize().X/2, DisplayServer.WindowGetSize().Y/2);
 	}
-
 	
 	public override void _Process(double delta)
 	{
--- Globals/Singleton.cs
+++ Globals/Singleton.cs
@@ -5,7 +5,7 @@ public partial class Singleton : Node
 {
 	public class MapSizes
 	{
-		public static readonly Vector2 easy = new(8, 10);
+		public static readonly Vector2 easy = new(10, 8);
 		public static readonly Vector2 medium = new(12, 16);
 	}
 }
