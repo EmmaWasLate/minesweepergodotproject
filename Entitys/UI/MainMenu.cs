ca7c9b43c4b430c9c33173aba7b0867b2fc6bb92
IsBelaHere <isabelasszhere@gmail.com>
2026-01-27 03:11:18 -0300

    Update Singleton and MainMenu
    Added a dictionary: key as int and Value is a Vector2I. This dictionary returns the mapsize of the difficulty you passed as a int type
    
    also, added a function that simplifies this and makes it more readable.
    
    Check Singleton.MapSizes.DifficultyToMapSize

--- Entitys/UI/MainMenu.cs
+++ Entitys/UI/MainMenu.cs
@@ -3,13 +3,25 @@ using System;
 
 public partial class MainMenu : MarginContainer
 {
-	
-	public override void _Ready()
+	[ExportGroup("BaseMenu")]
+	[Export] public MarginContainer baseMenuContainer;	
+	[Export] public Button newGameButton;
+
+	[ExportGroup("DifficultyMenu")]
+	[Export] public MarginContainer difficultyMenuContainer;
+	[Export] public Button easyOption;
+	[Export] public Button mediumOption;
+	[Export] public Button hardOption;
+	[Export] public Button customOption;
+
+	public void _ButtonDown_NewGame()
 	{
+		baseMenuContainer.Visible = false;
+		difficultyMenuContainer.Visible = true;
 	}
 
-	
-	public override void _Process(double delta)
+	public void _ButtonDown_SelectDifficulty(int difficulty)
 	{
+		
 	}
 }
--- Globals/Singleton.cs
+++ Globals/Singleton.cs
@@ -1,11 +1,28 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class Singleton : Node
 {
+	// organiza as dificuldades em numeros inteiros, começando no 0.
+	public static Dictionary<int, Vector2I> gameDifficulty = new Dictionary<int, Vector2I>
+	{
+		{0 , MapSizes.easy},
+		{1 , MapSizes.medium},
+	};
+			
 	public class MapSizes
 	{
 		public static readonly Vector2I easy = new(10, 8);
 		public static readonly Vector2I medium = new(16, 12);
+		
+		
+		
+		// retorna o tamanho de um mapa de acordo com a dificuldade passada.
+		// faz o mesmo que dictionary<Tkey,TValue>[key], porem mais legivel.
+		public static Vector2I DifficultyToMapSize(int difficulty)
+		{
+			return gameDifficulty[difficulty];
+		}	
 	}
 }
