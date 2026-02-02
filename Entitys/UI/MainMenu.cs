ee645e55a1db0434bb9da9e7c4f3710bea2cf861
IsBelaHere <isabelasszhere@gmail.com>
2026-02-01 21:02:23 -0300

    Custom difficulty completed

--- Entitys/UI/MainMenu.cs
+++ Entitys/UI/MainMenu.cs
@@ -17,6 +17,16 @@ public partial class MainMenu : MarginContainer
 	string ColumnsBoxPath = "%ColumsBox";
 	string BombsBoxPath = "%BombsBox";
 
+	SpinBox rowsBox;
+	SpinBox columsBox;
+	SpinBox bombsBox;
+    public override void _Ready()
+    {
+		rowsBox = GetNode<SpinBox>(rowsBoxPath);
+		columsBox = GetNode<SpinBox>(ColumnsBoxPath);
+		bombsBox = GetNode<SpinBox>(BombsBoxPath);
+    }
+
 	public void _ButtonDown_NewGame()
 	{
 		baseMenuContainer.Visible = false;
@@ -25,21 +35,34 @@ public partial class MainMenu : MarginContainer
 
 	public void _ButtonDown_SelectDifficulty(int difficulty)
 	{
+		if (difficulty == (int)Singleton.Difficulty.custom)
+		{
+			difficultyMenuContainer.Visible = false;
+			customDifficultyMenuContainer.Visible = true;
+			return;
+		}
 		GetNode<SceneManager>("/root/SceneManager").StartMainLevel((Singleton.Difficulty)difficulty);
 	}
 
 	public void _ButtonDown_CustomDifficulty()
 	{
-		SpinBox rowsBox = GetNode<SpinBox>(rowsBoxPath);
-		SpinBox columsBox = GetNode<SpinBox>(ColumnsBoxPath);
-		SpinBox bombsBox = GetNode<SpinBox>(BombsBoxPath);
 
 		Vector2I mapSize = new((int)rowsBox.Value,(int)columsBox.Value);
 		Singleton.DifficultyInfo.custom = new(mapSize,(int)bombsBox.Value);
 
 		Singleton.DifficultyInfo.UpdateDictionaryObj();
-
-		GetNode<SceneManager>("/root/SceneManager").StartMainLevel(Singleton.Difficulty.custom);
 		
+		GetNode<SceneManager>("/root/SceneManager").StartMainLevel(Singleton.Difficulty.custom);
+	}
+
+	public void _ValueChanged_ValuesWarn(float _)
+	{
+		Label smallSizeWarn = GetNode<Label>("%SmallSizeWarn");
+		Label bombsWarn = GetNode<Label>("%BombsWarn");
+
+		GD.Print(smallSizeWarn.Name);
+		GD.Print(bombsWarn.Name);
+		smallSizeWarn.Visible = rowsBox.Value * columsBox.Value < 2;
+		bombsWarn.Visible = bombsBox.Value >= rowsBox.Value * columsBox.Value;
 	}
 }
